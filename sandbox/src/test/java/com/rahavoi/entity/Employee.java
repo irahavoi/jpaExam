8b3db853ece47148e4257bd356d370c859a2f810
unknown <irahavoi@gmail.com>
2015-01-21 21:22:07 -0600

    simple entity example

---
+++ sandbox/src/test/java/com/rahavoi/entity/Employee.java
@@ -0,0 +1,61 @@
+package com.rahavoi.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	
+	@Id
+	private int id;
+	private String name;
+	private long salary;
+	
+	public Employee(){}
+
+	public Employee(int id){
+		
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the salary
+	 */
+	public long getSalary() {
+		return salary;
+	}
+
+	/**
+	 * @param salary the salary to set
+	 */
+	public void setSalary(long salary) {
+		this.salary = salary;
+	}
+}

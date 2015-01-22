74b7712ba1eb7c4f1846355ebd975667610fe608
unknown <irahavoi@gmail.com>
2015-01-21 21:50:30 -0600

    ::up

--- sandbox/src/main/java/com/rahavoi/sanbox/App.java
+++ sandbox/src/main/java/com/rahavoi/sanbox/App.java
@@ -1,5 +1,6 @@
 package com.rahavoi.sanbox;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
@@ -12,5 +13,12 @@ public class App
     public static void main( String[] args )
     {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeFactory");
+        EntityManager em = emf.createEntityManager();
+        
+        Employee employee = new Employee(11);
+        
+        em.persist(employee);
+        
+        
     }
 }

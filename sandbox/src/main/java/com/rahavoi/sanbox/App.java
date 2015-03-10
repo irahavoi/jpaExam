8cf3128a0be2b903508205c994cea031050cccda
unknown <irahavoi@gmail.com>
2015-03-09 22:56:46 -0500

    Up

--- sandbox/src/main/java/com/rahavoi/sanbox/App.java
+++ sandbox/src/main/java/com/rahavoi/sanbox/App.java
@@ -1,8 +1,11 @@
 package com.rahavoi.sanbox;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 /**
  * Hello world!
@@ -14,14 +17,19 @@ public class App
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("EmployeeFactory");
         EntityManager em = emf.createEntityManager();
         
-        Employee employee = new Employee();
-        employee.setName("Pokahontas");
-        employee.setSalary(45000);
+       // Employee employee = new Employee();
+        //employee.setName("Pokahontas");
+        //employee.setSalary(45000);
         
-        em.getTransaction().begin();
-        em.persist(employee);
+        //em.getTransaction().begin();
+        //em.persist(employee);
         //Employee e0 = em.find(Employee.class, 0);
         //em.remove(employee);
-        em.getTransaction().commit();        
+        //em.getTransaction().commit();
+        
+        TypedQuery<Employee> query = em.createQuery("SELECT e FROM Employee e", Employee.class);
+        List<Employee> employees = query.getResultList();
+        
+        System.out.println("Found " + employees.size() + " employees.");
     }
 }

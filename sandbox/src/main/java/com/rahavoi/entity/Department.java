ffc3540a5929074d236fce0acaa1a7f1b9d730ab
unknown <irahavoi@gmail.com>
2015-03-20 23:36:39 -0500

    up

--- sandbox/src/main/java/com/rahavoi/entity/Department.java
+++ sandbox/src/main/java/com/rahavoi/entity/Department.java
@@ -1,9 +1,12 @@
 package com.rahavoi.entity;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Department {
@@ -12,6 +15,10 @@ public class Department {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private int id;
 	private String name;
+	
+	@OneToMany(mappedBy="department")
+	private Collection<Employee> employees;
+	
 	/**
 	 * @return the id
 	 */
@@ -31,6 +38,22 @@ public class Department {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	/**
+	 * @return the employees
+	 */
+	public Collection<Employee> getEmployees() {
+		return employees;
+	}
+
+	/**
+	 * @param employees the employees to set
+	 */
+	public void setEmployees(Collection<Employee> employees) {
+		this.employees = employees;
+	}
+	
+	
 	
 	
 	
--- sandbox/src/main/java/com/rahavoi/sanbox/App.java
+++ sandbox/src/main/java/com/rahavoi/sanbox/App.java
@@ -1,5 +1,6 @@
 package com.rahavoi.sanbox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -7,11 +8,21 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
 
+import com.rahavoi.entity.Department;
 import com.rahavoi.entity.Employee;
+import com.rahavoi.entity.ParkingSpace;
 import com.rahavoi.sanbox.service.EmployeeService;
 
 /**
  * Hello world!
+ * 
+ * The two rules, then, for bidirectional one-to-one associations are the following:
+	• The @JoinColumn annotation goes on the mapping of the entity that is mapped to
+	the table containing the join column, or the owner of the relationship. This might
+	be on either side of the association.
+	• The mappedBy element should be specified in the @OneToOne annotation in the
+	entity that does not define a join column, or the inverse side of the relationship.
+
  */
 public class App 
 {
@@ -22,21 +33,34 @@ public class App
         
     	EmployeeService eService = new EmployeeService(em);
         
-       // Employee employee = new Employee();
-        //employee.setName("Pokahontas");
-        //employee.setSalary(45000);
-        
-        //em.getTransaction().begin();
-        //em.persist(employee);
-        //Employee e0 = em.find(Employee.class, 0);
-        //em.remove(employee);
-        //em.getTransaction().commit();
-        
-        List<Employee> employees = eService.findAllEmployees();
-        
-        em.close();
-    	emf.close();
-        
-        System.out.println("Found " + employees.size() + " employees.");
-    }
+       Employee employee = new Employee();
+       employee.setName("Pokahontas");
+       employee.setSalary(45000);
+       
+       ParkingSpace parking = new ParkingSpace();
+       parking.setLocation("5th floor");
+       parking.setLot(100);
+       
+       Department department = new Department();
+       department.setName("IT");
+       
+       employee.setDepartment(department);
+       employee.setParkingSpace(parking);
+       
+       List<Employee> allEmployees = new ArrayList<Employee>();
+       department.setEmployees(allEmployees);
+       
+       em.getTransaction().begin();
+       
+       em.persist(employee);
+       em.persist(department);
+       em.persist(parking);
+       
+       
+       em.getTransaction().commit();
+       
+       em.close();
+       emf.close();
+       
+   }
 }
